03c1b4c9430a601c5be316aa75dc10f9a21fba78
Montchat <je4440@gmail.com>
2015-10-19 14:15:30 -0400

    playing with the camera
    - setting up camera functionality

--- Camera/Camera/ViewController.m
+++ Camera/Camera/ViewController.m
@@ -7,21 +7,64 @@
 //
 
 #import "ViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property UIImagePickerController * pickerController;
+
+@property (weak, nonatomic) IBOutlet UIImageView *mySelfieImageView;
 
 @end
 
 @implementation ViewController
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self != nil) {
+        self.pickerController = [[UIImagePickerController alloc] init];
+        self.pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        self.pickerController.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+        self.pickerController.delegate = self;
+
+    }
+    
+    return self;
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    if (self.mySelfieImageView.image == nil) {
+        [self presentViewController:self.pickerController animated:YES completion:nil];
+        
+    }
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    NSString * type = info[UIImagePickerControllerMediaType];
+    
+    if (type == (NSString *)kUTTypeImage) {
+        UIImage * image = info[UIImagePickerControllerOriginalImage];
+        
+        self.mySelfieImageView.image = image;
+        
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
