e9ba4b19b16f9475f1249dfe18baa9a79b763151
Montchat <je4440@gmail.com>
2015-10-19 09:28:29 -0400

    Created Project
    -Created Project

---
+++ Camera/Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Joe E. on 10/19/15.
+//  Copyright © 2015 Joe E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
